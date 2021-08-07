8e3255e3d8fcc291e5453859eb6c6ccda76cb473
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2021-08-07 14:23:28 +0100

    File change, 1st Font added, 1st responsivness

--- home.js
+++ home.js
@@ -1,3 +1,8 @@
-const images = []
-const links = document.getElementById("project-list").querySelectorAll("a")
-const img = document.getElementById(project-img)
+const links = document.getElementById('project-list').querySelectorAll('a')
+const img = document.getElementById('project-img')
+
+links[0].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/hapa.jpg")'}
+links[1].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/ama.png")'}
+links[2].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/plastic.JPG")'}
+links[3].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/foodporn.jpg")'}
+links[4].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/librarycard.JPG")'}
