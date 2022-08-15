e22ee7a51351b5659eea5c213864f9cf111261da
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2022-08-15 22:07:08 +0100

    sizing fixes etc

--- home.js
+++ home.js
@@ -1,8 +1,7 @@
 const links = document.getElementById('project-list').querySelectorAll('a')
 const img = document.getElementById('project-img')
 
-links[0].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/hapa.jpg")'}
-links[1].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/ama.png")'}
-links[2].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/plastic.JPG")'}
-links[3].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/foodporn.jpg")'}
-links[4].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/librarycard.JPG")'}
+links[0].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/tutor_the_nation_cover.png")'}
+links[1].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/drew_cover.png")'}
+links[2].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/danclairs_cover.png")'}
+links[3].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/amas_cover.png")'}
