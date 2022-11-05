27880e131bed532440629a0281cd83c42d197472
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2022-11-05 18:28:26 +0000

    Mobile homepage complete

--- home.js
+++ home.js
@@ -1,7 +1,18 @@
 const links = document.getElementById('project-list').querySelectorAll('a')
 const img = document.getElementById('project-img')
 
-links[0].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/tutor_the_nation_cover.png")'}
-links[1].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/drew_cover.png")'}
-links[2].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/danclairs_cover.png")'}
-links[3].onmouseover = () => {img.style.backgroundImage = 'url("Images/Home/amas_cover.png")'}
+links[0].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/tutor_the_nation_cover.png")' }
+links[1].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/drew_cover.png")' }
+links[2].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/danclairs_cover.png")' }
+links[3].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/amas_cover.png")' }
+
+const menuButton = document.getElementById('menu-button')
+const sbLogo = document.getElementById('SB-logo')
+const mobileMenuLinks = document.getElementById('mobile-menu-links')
+
+console.log(menuButton, sbLogo,)
+const toggleMenu = () => {
+  sbLogo.classList.toggle('hidden')
+  mobileMenuLinks.classList.toggle('hidden')
+}
+
