50bbd43ba0d46c4ff718dc5dc8f286bbc1c2e2cc
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2022-11-24 11:00:25 +0000

    home page image updates

--- home.js
+++ home.js
@@ -1,7 +1,17 @@
 const links = document.getElementById('project-list').querySelectorAll('a')
 const img = document.getElementById('project-img')
 
-links[0].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/tutor_the_nation_cover.png")' }
-links[1].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/drew_cover.png")' }
-links[2].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/danclairs_cover.png")' }
-links[3].onmouseover = () => { img.style.backgroundImage = 'url("Images/Home/amas_cover.png")' }
+links[0].onmouseover = () => {
+  img.style.backgroundImage = 'url("Images/Home/tutor-the-nation-cover.png")'
+  img.style.backgroundColor = ''
+}
+links[1].onmouseover = () => {
+  img.style.backgroundImage = 'url("Images/Home/drew_cover.png")'
+}
+links[2].onmouseover = () => {
+  img.style.backgroundImage = 'url("Images/Home/danclairs-cover.png")'
+  img.style.backgroundColor = '#ECE6D4'
+}
+links[3].onmouseover = () => {
+  img.style.backgroundImage = 'url("Images/Home/amas_cover.png")'
+}
