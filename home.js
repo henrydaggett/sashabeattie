ea4dfd25d87a306992cfbee3530a34aec2bb91b7
Henry Daggett <34757031+henrydaggett@users.noreply.github.com>
2022-11-24 16:21:05 +0000

    Init. bond vigilantes

--- home.js
+++ home.js
@@ -10,10 +10,14 @@ links[1].onmouseover = () => {
   img.style.backgroundColor = ''
 }
 links[2].onmouseover = () => {
+  img.style.backgroundImage = 'url("Images/Home/bond-vigi-cover.png")'
+  img.style.backgroundColor = ''
+}
+links[3].onmouseover = () => {
   img.style.backgroundImage = 'url("Images/Home/danclairs-cover.png")'
   img.style.backgroundColor = '#ECE6D4'
 }
-links[3].onmouseover = () => {
+links[4].onmouseover = () => {
   img.style.backgroundImage = 'url("Images/Home/amas_cover.png")'
   img.style.backgroundColor = '#EF856F'
 }
